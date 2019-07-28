973be4704a0eec7874e780bd1f8967d161b57c69
Mohammad Samir <mohammad@pop-os.localdomain>
2019-07-28 03:16:05 +0200

    hacking allowed hosts to all by using *

--- LMS/settings.py
+++ LMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ql6+b^1-f6!-+qk*an%0f8(5p0c9d)w69zmwl$tcy#-dsc%d&u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
